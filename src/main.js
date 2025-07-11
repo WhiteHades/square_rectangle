3f11b719a4a79fd198a65ff56d4db0b1b328a25f
WhiteHades <44260523+WhiteHades@users.noreply.github.com>
2025-07-11 19:10:15 +0200

    scaffold dependencies

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
+import './assets/tailwind.css'
+//import './style.css'
 import App from './App.vue'
 
+import { MotionPlugin } from '@vueuse/motion'
+import { SparkUI } from 'spark-ui'
+import 'swiper/css'
+import 'swiper/css/thumbs'
+import 'swiper/css/navigation'
+
 createApp(App).mount('#app')
+
+App.use(MotionPlugin)
+App.use(SparkUI)
+
